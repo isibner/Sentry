99dde671f2f2c170f19bd1b82717dd2ceda841b9
Fabio Fleitas <cubanfabio@gmail.com>
2015-01-17 01:06:39 -0500

    added post request for github webhook

--- app/routes/github.js
+++ app/routes/github.js
@@ -4,5 +4,6 @@ var github = require('../controllers/github');
 var router = express.Router();
 
 router.get('/hooks/github', github.webhook);
+router.post('/hooks/github', github.webhook);
 
 module.exports = router;
