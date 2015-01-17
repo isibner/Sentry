314c418ef4e52ae66486bdbe9463909930156aaf
Fabio Fleitas <cubanfabio@gmail.com>
2015-01-17 16:11:46 -0500

    working on landing page

--- config/config.js
+++ config/config.js
@@ -9,8 +9,8 @@ module.exports = {
   mongo: {
     uri: process.env.MONGOLAB_URI || 'mongodb://localhost/node-boiler'
   },
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || 'wat',
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET || 'wat',
 
   BOT_USERNAME: process.env.BOT_USERNAME,
   BOT_PASSWORD: process.env.BOT_PASSWORD,
